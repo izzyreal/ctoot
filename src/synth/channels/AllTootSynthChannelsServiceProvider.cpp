2752efc97cdd4700fb5e13da2292db524bf8ea2f
Izmar Verhage <izmaelverhage@gmail.com>
2018-04-02 17:12:03 +0200

    Added MPC channels to service provision

--- src/synth/channels/AllTootSynthChannelsServiceProvider.cpp
+++ src/synth/channels/AllTootSynthChannelsServiceProvider.cpp
@@ -13,6 +13,12 @@
 #include <synth/channels/valor/ValorSynthControls.hpp>
 //#include <synth/channels/whirl/WhirlSynthChannel.hpp>
 //#include <synth/channels/whirl/WhirlSynthControls.hpp>
+
+#include <mpc/MpcBasicSoundPlayerChannel.hpp>
+#include <mpc/MpcBasicSoundPlayerControls.hpp>
+#include <mpc/MpcSoundPlayerChannel.hpp>
+#include <mpc/MpcSoundPlayerControls.hpp>
+
 #include <synth/id/TootSynthControlsId.hpp>
 
 using namespace ctoot::synth::channels;
@@ -24,7 +30,10 @@ AllTootSynthChannelsServiceProvider::AllTootSynthChannelsServiceProvider()
 	std::string name;
     name = ctoot::synth::channels::valor::ValorSynthControls::NAME;
     addControls("class ctoot::synth::channel::SynthChannelControls", ctoot::synth::id::TootSynthControlsId::VALOR_CHANNEL_ID, "class ctoot::synth::channels::valor::ValorSynthControls", "Virtual Analog Polyphonic", "0.2");
-    //add(ctoot::synth::channels::valor::ValorSynthChannel::class_(), name, u"Valor", u"0.2");
+	addControls("class ctoot::mpc::MpcBasicSoundPlayerControls", ctoot::mpc::MpcBasicSoundPlayerControls::MPC_BASIC_SOUND_PLAYER_CHANNEL_ID, "class ctoot::mpc::MpcBasicSoundPlayerControls", "MPC Basic", "0.2");
+	addControls("class ctoot::mpc::MpcSoundPlayerControls", ctoot::mpc::MpcSoundPlayerControls::MPC_SOUND_PLAYER_CHANNEL_ID, "class ctoot::mpc::MpcSoundPlayerControls", "MPC", "0.2");
+
+	//add(ctoot::synth::channels::valor::ValorSynthChannel::class_(), name, u"Valor", u"0.2");
 
 	//addControls("class ctoot::audio::core::AudioControls", DelayIds::MODULATED_DELAY_ID, "class ctoot::audio::delay::ModulatedDelayControls", family, "0.2");
 
@@ -47,27 +56,34 @@ AllTootSynthChannelsServiceProvider::AllTootSynthChannelsServiceProvider()
 	*/
 }
 
- shared_ptr<ctoot::synth::SynthChannel> AllTootSynthChannelsServiceProvider::createSynthChannel(weak_ptr<ctoot::synth::SynthChannelControls> c)
+shared_ptr<ctoot::synth::SynthChannel> AllTootSynthChannelsServiceProvider::createSynthChannel(weak_ptr<ctoot::synth::SynthChannelControls> c)
 {
-	 auto name = c.lock()->getName();
-	 MLOG("AllTootSynthChannelServiceProvider::createSynthChannel c name: " + name);
-	 if (name.compare("Valor") == 0) {
-		 return make_shared<ctoot::synth::channels::valor::ValorSynthChannel>(dynamic_pointer_cast<ctoot::synth::channels::valor::ValorSynthControls>(c.lock()).get());
-	 }
-	/*
-	 if(dynamic_cast< ctoot::synth::channels::valor::ValorSynthControls* >(c) != nullptr) {
-        return new ctoot::synth::channels::valor::ValorSynthChannel(java_cast< ctoot::synth::channels::valor::ValorSynthControls* >(c));
-    } else if(dynamic_cast< ctoot::synth::channels::pluck::PluckSynthControls* >(c) != nullptr) {
-        return new ctoot::synth::channels::pluck::PluckSynthChannel(java_cast< ctoot::synth::channels::pluck::PluckSynthControls* >(c));
-    } else if(dynamic_cast< ctoot::synth::channels::copal::CopalSynthControls* >(c) != nullptr) {
-        return new ctoot::synth::channels::copal::CopalSynthChannel(java_cast< ctoot::synth::channels::copal::CopalSynthControls* >(c));
-    } else if(dynamic_cast< ctoot::synth::channels::whirl::WhirlSynthControls* >(c) != nullptr) {
-        return new ctoot::synth::channels::whirl::WhirlSynthChannel(java_cast< ctoot::synth::channels::whirl::WhirlSynthControls* >(c));
-    } else if(dynamic_cast< ctoot::synth::channels::total::TotalSynthControls* >(c) != nullptr) {
-        return new ctoot::synth::channels::total::TotalSynthChannel(java_cast< ctoot::synth::channels::total::TotalSynthControls* >(c));
-    } else if(dynamic_cast< ctoot::synth::channels::nine::NineSynthControls* >(c) != nullptr) {
-        return new ctoot::synth::channels::nine::NineSynthChannel(java_cast< ctoot::synth::channels::nine::NineSynthControls* >(c));
-    }
-	*/
-	 return {};
+	auto name = c.lock()->getName();
+	MLOG("AllTootSynthChannelServiceProvider::createSynthChannel c name: " + name);
+	if (name.compare("Valor") == 0) {
+		return make_shared<ctoot::synth::channels::valor::ValorSynthChannel>(dynamic_pointer_cast<ctoot::synth::channels::valor::ValorSynthControls>(c.lock()).get());
+	}
+	if (name.compare("MpcBasicSoundPlayerChannel") == 0) {
+		return make_shared<ctoot::mpc::MpcBasicSoundPlayerChannel>(dynamic_pointer_cast<ctoot::mpc::MpcBasicSoundPlayerControls>(c.lock()));
+	}
+	if (name.compare("MpcSoundPlayer") == 0) {
+		return make_shared<ctoot::mpc::MpcSoundPlayerChannel>(dynamic_pointer_cast<ctoot::mpc::MpcSoundPlayerControls>(c.lock()));
+	}
+
+		/*
+		 if(dynamic_cast< ctoot::synth::channels::valor::ValorSynthControls* >(c) != nullptr) {
+			return new ctoot::synth::channels::valor::ValorSynthChannel(java_cast< ctoot::synth::channels::valor::ValorSynthControls* >(c));
+		} else if(dynamic_cast< ctoot::synth::channels::pluck::PluckSynthControls* >(c) != nullptr) {
+			return new ctoot::synth::channels::pluck::PluckSynthChannel(java_cast< ctoot::synth::channels::pluck::PluckSynthControls* >(c));
+		} else if(dynamic_cast< ctoot::synth::channels::copal::CopalSynthControls* >(c) != nullptr) {
+			return new ctoot::synth::channels::copal::CopalSynthChannel(java_cast< ctoot::synth::channels::copal::CopalSynthControls* >(c));
+		} else if(dynamic_cast< ctoot::synth::channels::whirl::WhirlSynthControls* >(c) != nullptr) {
+			return new ctoot::synth::channels::whirl::WhirlSynthChannel(java_cast< ctoot::synth::channels::whirl::WhirlSynthControls* >(c));
+		} else if(dynamic_cast< ctoot::synth::channels::total::TotalSynthControls* >(c) != nullptr) {
+			return new ctoot::synth::channels::total::TotalSynthChannel(java_cast< ctoot::synth::channels::total::TotalSynthControls* >(c));
+		} else if(dynamic_cast< ctoot::synth::channels::nine::NineSynthControls* >(c) != nullptr) {
+			return new ctoot::synth::channels::nine::NineSynthChannel(java_cast< ctoot::synth::channels::nine::NineSynthControls* >(c));
+		}
+		*/
+		return {};
 }
