c1d8b52869fe1f5220660154ffdda20506716aaf
Izmar <izmaelverhage@gmail.com>
2019-03-09 18:00:54 +0100

    Adds conan test package

--- conanfile.py
+++ conanfile.py
@@ -18,3 +18,6 @@ class Pkg(ConanFile):
         self.copy("*.hpp", src="src/main", dst="include")
         self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs=["ctoot"]
