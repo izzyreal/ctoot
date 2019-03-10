6b1d95a42b189ec8b18f648933f53dfbdf5a27f8
Izmar <izmaelverhage@gmail.com>
2019-03-10 11:48:21 +0100

    Adds Catch2 tests

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -1,9 +1,26 @@
-#include "../main/audio/core/AudioFormat.hpp"
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
 
-int main(){
-	auto af = new ctoot::audio::core::AudioFormat(44100, 16, 2, false, false);
-	float sr = af->getSampleRate();
-	printf("Samplerate is %f\n", sr);
+#include "../main/audio/core/AudioFormat.hpp"
+#include "../main/audio/core/AudioServices.hpp"
+
+using namespace ctoot::audio::core;
+
+TEST_CASE("AudioFormat is instantiated", "[audioformat]") {
+	const auto sr1 = 44100.f;
+	auto af = new AudioFormat(sr1, 16, 2, false, false);
+	const auto sr2 = af->getSampleRate();
 	delete af;
-	printf("ctoot test successful\n");
+	REQUIRE(sr1 == sr2);
+}
+
+TEST_CASE("AudioServices are scanned", "[audioservices-scan]") {
+	std::string testControlName = "ctoot::audio::analysis::SpectrumAnalyserControls";
+	AudioServices::scan();
+	auto ac = AudioServices::getAvailableControls();
+	REQUIRE(ac.find(testControlName) != std::string::npos);
+	auto testControl = AudioServices::createControls(testControlName);
+	REQUIRE(testControl);
+	auto name = testControl->getName();
+	REQUIRE(name.compare("SpectrumAnalyser") == 0);
 }
