63d8c4a0275c78e0ab7e0616318b90940bd56edb
Izmar Verhage <izmaelverhage@gmail.com>
2020-04-29 01:24:33 +0300

    Add debug to build script

--- rebuild.sh
+++ rebuild.sh
@@ -1,8 +1,17 @@
 rm -rf ~/.conan/data/ctoot
+
+mkdir -p ~/git/ctoot/build/Release
+mkdir -p ~/git/ctoot/build/Debug
+
 rm -rf ~/git/ctoot/build/Release/*
+rm -rf ~/git/ctoot/build/Debug/*
+
+cd ~/git/ctoot/build/Debug
+conan install -s build_type=Debug ../../ ctoot/0.1@izmar/dev --build missing
+conan build ../../
 
 cd ~/git/ctoot/build/Release
-
 conan install -s build_type=Release ../../ ctoot/0.1@izmar/dev --build missing
 conan build ../../
+
 conan create ../../ ctoot/0.1@izmar/dev
