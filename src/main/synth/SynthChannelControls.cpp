814ad1115431d6a9ad7e33db10cb08490db1afb2
Izmar Verhage <izmaelverhage@gmail.com>
2023-03-12 01:38:02 +0100

    Remove more synth stuff

--- src/main/synth/SynthChannelControls.cpp
+++ src/main/synth/SynthChannelControls.cpp
@@ -14,48 +14,4 @@ SynthChannelControls::SynthChannelControls(int id, string name)
 
 void SynthChannelControls::close()
 {
-	if (observable != nullptr) {
-		observable->deleteObserver(this);
-	}
-}
-
-void SynthChannelControls::notifyParent(Control* obj)
-{
-    if(learn) {
-        learnControl = obj;
-    }
-    CompoundControl::notifyParent(obj);
-}
-
-void SynthChannelControls::ensureMapExists()
-{
-	if (map.size() != 128) map = vector<Control*>(128);
-}
-
-void SynthChannelControls::update(moduru::observer::Observable* o, nonstd::any a)
-{
-
-	if (dynamic_cast<ControlChange*>(o) == nullptr) return;
-	if (observable == nullptr) observable = o;
-	auto change = dynamic_cast<ControlChange*>(o);
-	auto controller = change->getController();
-	if (controller >= 32 && controller < 64) return;
-
-	if (learn && learnControl != nullptr) {
-		ensureMapExists();
-		map.insert(map.begin() + controller, learnControl);
-		learn = false;
-		learnControl = nullptr;
-	}
-	if (map.size() == 128 && map[controller] != nullptr) {
-		changeQueue.try_enqueue(change);
-	}
-}
-
-SynthChannelControls::~SynthChannelControls() {
-	for (auto& c : map) {
-		if (c != nullptr) {
-			delete c;
-		}
-	}
 }
--- src/main/synth/SynthChannelControls.hpp
+++ src/main/synth/SynthChannelControls.hpp
@@ -3,37 +3,16 @@
 #include <synth/ControlChange.hpp>
 #include <control/CompoundControl.hpp>
 
-#include <observer/Observer.hpp>
-#include <thirdp/concurrentqueue.h>
-
 namespace ctoot::synth {
 
     class SynthChannelControls
-            : public ctoot::control::CompoundControl, public moduru::observer::Observer
+            : public ctoot::control::CompoundControl
     {
-
-    private:
-        bool learn{false};
-        ctoot::control::Control *learnControl{nullptr};
-        std::vector<ctoot::control::Control *> map{};
-        moodycamel::ConcurrentQueue<ControlChange *> changeQueue{};
-        moduru::observer::Observable *observable{nullptr};
-
     public:
         void close() override;
 
     public:
-        void notifyParent(ctoot::control::Control *obj) override;
-
-        virtual void ensureMapExists();
-
-    public:
-        void update(moduru::observer::Observable *o, nonstd::any a) override;
-
-    public:
         SynthChannelControls(int id, std::string name);
-
-        virtual ~SynthChannelControls();
 
     };
 
